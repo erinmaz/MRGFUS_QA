b8bb22acb737d99559542ce44e74ec46111393cf
Erin Mazerolle <erinmaz@gmail.com>
2020-07-13 13:47:48 -0300

    new script for QA when I don't have matlab

--- QA_nomatlab.sh
+++ QA_nomatlab.sh
@@ -6,7 +6,7 @@ MYSUB=$1
 
 ##### CHECK: DO YOU NEED TO EDIT THESE VARIABLES?
 MAINDIR=/Users/erin/Desktop/Projects/MRGFUS
-SPMDIR=/Users/erin/Documents/MATLAB/spm12
+
 ##################################################
 
 
@@ -56,14 +56,7 @@ done
 dcm2niix -z y -b n -f %d_s%s_e%e ${T1dir}
 mv ${T1dir}/*.nii.gz ${ANATDIR}/T1.nii.gz
 
-#SPM Segment
-fslchfiletype NIFTI ${ANATDIR}/T1
-export MATLABPATH="${SPMDIR}:${SCRIPTSDIR}"
-nice matlab -nosplash -nodesktop -r "segment_job({'${ANATDIR}/T1.nii,1'}) ; quit"
-fslchfiletype NIFTI_GZ ${ANATDIR}/T1
-fslmaths ${ANATDIR}/c1T1 -add ${ANATDIR}/c2T1 -add ${ANATDIR}/c3T1 -bin -fillh ${ANATDIR}/spm_mask
-fslmaths ${ANATDIR}/T1 -mas ${ANATDIR}/spm_mask ${ANATDIR}/T1_brain
-fsleyes ${ANATDIR}/T1 ${ANATDIR}/spm_mask &
+
 
 #################### T2 QA #############################################
 for f in ${DICOMDIR}/${MYSUB}/*PUSag_CUBE_T2*; do
@@ -190,73 +183,13 @@ fi
 mkdir ${ANALYSISDIR}/${MYSUB}/diffusion/xfms
 #flirt -in $diffforreg -ref $T1fordiffreg -omat ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff2str.mat -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 6 -cost corratio -out ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff2str 
 
-epi_reg --epi=${diffforreg} --t1=${T1fordiffreg} --t1brain=${T1fordiffreg}_brain --out=${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff_2_T1_bbr
-convert_xfm -omat ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/T1_2_diff_bbr.mat -inverse ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff_2_T1_bbr.mat
-fsleyes ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff_2_T1_bbr ${T1fordiffreg} &
 
 #fsleyes ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff2str $T1fordiffreg &
 #convert_xfm -omat ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/str2diff.mat -inverse ${ANALYSISDIR}/${MYSUB}/diffusion/xfms/diff2str.mat 
 
 	
-############## fMRI QA ################################################
-for f in ${DICOMDIR}/${MYSUB}/*PUrsBOLD*; do
-    if [ -e "$f" ]; then
-		PUREBOLD=YES
-		BOLDdir=$f
-    else
-		PUREBOLD=NO
-		BOLDdir=${DICOMDIR}/${MYSUB}/*rsBOLD*
-    fi
-    break
-done
-mkdir ${ANALYSISDIR}/${MYSUB}/fmri
-dcm2niix -z y -b n -f %d_s%s_e%e ${BOLDdir}
-mv ${BOLDdir}/*.nii.gz ${ANALYSISDIR}/${MYSUB}/fmri/rs.nii.gz
-fsleyes ${ANALYSISDIR}/${MYSUB}/fmri/rs &
-sed 's:MYINPUT:'${ANALYSISDIR}'/'${MYSUB}'/fmri/rs:g' ${SCRIPTSDIR}/QA_${MYCOIL}.fsf > ${ANALYSISDIR}/${MYSUB}/fmri/QA_${MYCOIL}.fsf
-feat ${ANALYSISDIR}/${MYSUB}/fmri/QA_${MYCOIL}.fsf
-fslmaths ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/filtered_func_data -Tstd ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/std_func
-fslmaths ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/mean_func -div ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/std_func ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/tsnr_func
-rstsnr=`fslstats ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/tsnr_func -k ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/mask -M`
-fsleyes ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/tsnr_func &
-fsleyes ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/filtered_func_data.ica/melodic_IC &
-fsl_motion_outliers -i ${ANALYSISDIR}/${MYSUB}/fmri/rs -s ${ANALYSISDIR}/${MYSUB}/fmri/rs_motion.rms -p ${ANALYSISDIR}/${MYSUB}/fmri/rs_motion.png -o ${ANALYSISDIR}/${MYSUB}/fmri/rs_motion_confounds.txt
-sed 's:MYINPUT:'${ANALYSISDIR}'/'${MYSUB}'/fmri/rs:g' ${SCRIPTSDIR}/QA_mc_only_${MYCOIL}.fsf > ${ANALYSISDIR}/${MYSUB}/fmri/QA_mc_only_${MYCOIL}.fsf
-feat ${ANALYSISDIR}/${MYSUB}/fmri/QA_mc_only_${MYCOIL}.fsf
-fslmaths ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/filtered_func_data -Tstd ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/std_func
-fslmaths ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/mean_func -div ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/std_func ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/tsnr_func
-rstsnr_mc_only=`fslstats ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/tsnr_func -k ${ANALYSISDIR}/${MYSUB}/fmri/rs.feat/mask -M`
-fsleyes ${ANALYSISDIR}/${MYSUB}/fmri/rs+.feat/tsnr_func &
-if [ "$PURET1" = "YES" ] && [ "$PUREBOLD" = "YES" ]
-then
-	T1forreg=${ANATDIR}/T1_brain
-	BOLDforreg=${ANALYSISDIR}/${MYSUB}/fmri/rs
-elif [ "$PURET1" = "NO" ] && [ "$PUREBOLD" = "NO" ]
-then
-	T1forreg=${ANATDIR}/T1_brain
-	BOLDforreg=${ANALYSISDIR}/${MYSUB}/fmri/rs
-elif [ "$PURET1" = "YES" ]
-then
-#need to get non-PURE T1 to reg with BOLD
-	#not very efficient, because I potentially convert nonPURE T1 twice (once for fMRI and once for diffusion)
-	dcm2niix -z y -b n -f %d_s%s_e%e ${DICOMDIR}/${MYSUB}/*-SAG_FSPGR_BRAVO*
-	mv ${DICOMDIR}/${MYSUB}/*-SAG_FSPGR_BRAVO*/*.nii.gz ${ANATDIR}/T1_noPURE.nii.gz
-	fslmaths ${ANATDIR}/T1_noPURE -mas ${ANATDIR}/spm_mask ${ANATDIR}/T1_noPURE_brain
-	T1forreg=${ANATDIR}/T1_noPURE_brain
-	BOLDforreg=${ANALYSISDIR}/${MYSUB}/fmri/rs
-elif [ "$PUREBOLD" = "YES" ]
-then
-#need to get non-PURE BOLD to reg with T1
-	dcm2niix -z y -b n -f %d_s%s_e%e ${DICOMDIR}/${MYSUB}/*-rsBOLD*
-	mv ${DICOMDIR}/${MYSUB}/*-rsBOLD*/*.nii.gz ${ANALYSISDIR}/${MYSUB}/fmri/rs_noPURE.nii.gz
-	T1forreg=${ANATDIR}/T1_brain
-	BOLDforreg=${ANALYSISDIR}/${MYSUB}/fmri/rs_noPURE
-fi
-sed 's:MYINPUT:'${BOLDforreg}':g' ${SCRIPTSDIR}/reg_${MYCOIL}.fsf > ${ANALYSISDIR}/${MYSUB}/fmri/reg_${MYCOIL}.fsf
-sed -i "" 's:MYT1:'${T1forreg}':g' ${ANALYSISDIR}/${MYSUB}/fmri/reg_${MYCOIL}.fsf
-sed -i "" 's:MYOUTPUT:'${ANALYSISDIR}'/'${MYSUB}'/fmri/rs_reg.feat:g'  ${ANALYSISDIR}/${MYSUB}/fmri/reg_${MYCOIL}.fsf
-feat ${ANALYSISDIR}/${MYSUB}/fmri/reg_${MYCOIL}.fsf
+
 	
 
 ################# SUMMARY OUTPUT ########################################
-echo $MYSUB,$MYCOIL,$DATE,$STUDYINFO,$PURET1,$PURET2,$PUREdiff,$PUREBOLD,$difftsnr,`echo $diffcnr | tr ' ' ,` ,`awk '{ sum += $2; n++ } END { print sum / n; } ' ${ANALYSISDIR}/${MYSUB}/diffusion/data.eddy_restricted_movement_rms`,$rstsnr,$rstsnr_mc_only,`awk -v max=0 '{if($1>max){ max=$1}}END{print max} ' ${ANALYSISDIR}/${MYSUB}/fmri/rs_motion.rms`,`awk '{ total += $1 } END { print total/NR}' ${ANALYSISDIR}/${MYSUB}/fmri/rs_motion.rms`
+echo $MYSUB,$MYCOIL,$DATE,$STUDYINFO,$PURET1,$PURET2,$PUREdiff,$PUREBOLD,$difftsnr,`echo $diffcnr | tr ' ' ,` ,`awk '{ sum += $2; n++ } END { print sum / n; } ' ${ANALYSISDIR}/${MYSUB}/diffusion/data.eddy_restricted_movement_rms`,NC,NC,NC,NC
