bba28b82b4c81727822bfe0f44bbcf9378ea15c6
Erin Mazerolle <erinmaz@gmail.com>
2020-07-20 13:45:12 -0300

    removed subjects we're not analyzing from run_QA_nomatlab_2_segs_from_Conrad.sh

--- run_QA_nomatlab_2_segs_from_Conrad.sh
+++ run_QA_nomatlab_2_segs_from_Conrad.sh
@@ -5,8 +5,11 @@ SegsDir=/Users/erin/Desktop/Projects/MRGFUS/SPMsegs_from_Conrad_July2020
 #cp ${SegsDir}/${f}/anat/* /Users/erin/Desktop/Projects/MRGFUS/analysis/${f}/anat/.
 #QA_nomatlab_2.sh ${f}
 
-for f in 	9027_KB-16664	9028_PR-17283	9029_WW-17425	9031_DB-17261 9024_LLB-17182	9027_KB-17111	9029_WW-16753	9030_GA-17060	9031_DB-17340 9025_RR-16717	9028_PR-16523	9029_WW-16929	9030_GA-17082	9031_DB-17635 9027_KB-16392	9028_PR-16838	9029_WW-16938	9030_GA-17501
+for f in  9024_LLB-17182 9030_GA-17060	9031_DB-17340 9025_RR-16717	9028_PR-16523	9030_GA-17082	9031_DB-17635 99028_PR-16838	9030_GA-17501
 do
 cp ${SegsDir}/${f}/anat/* /Users/erin/Desktop/Projects/MRGFUS/analysis/${f}/anat/.
 QA_nomatlab_2.sh ${f} &> /Users/erin/Desktop/Projects/MRGFUS/logs/QA_nomatlab_2_${f}.log
 done
+
+#done already 9028_PR-17283 
+# need to fix T1 seg 9031_DB-17261
